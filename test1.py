ca16ad4c2eed49aa547db625cf94e634450037f3
Lan081 <propingu@gmail.com>
2019-06-28 19:43:42 +0200

    Error handling when specific team web doesn't found

--- test1.py
+++ test1.py
@@ -1,4 +1,4 @@
-import time
+#import time
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
@@ -15,29 +15,33 @@ TeamsList = ("atletico","barcelona","real-madrid","valencia","sevilla","espanyol
 "levante","rayo","valladolid","villarreal","real-sociedad","girona")
 
 urlbase = "https://www.marca.com/futbol/%s.html" 
-newurl = (urlbase,"atletico")
 for CurrentTeam in TeamsList:
     url = urlbase % (CurrentTeam)
     print("Retrieving news from %s..." %(CurrentTeam))
     
     response = requests.get(url)
+    
     soup = BeautifulSoup(response.text, 'html.parser')
+   
     good_html = soup.prettify()
     results = soup.find_all('ul', attrs={"class":"auto-items"})
-    headers = results[0].findAll('header',attrs={"class":"mod-header"})
-    for i in headers:    
-         hreflink = i.findAll('a')
-         Title = hreflink[0]['title']
-         Link = hreflink[0]['href']    
-         Team = CurrentTeam
-         StartDate = Link.find('2019')
-         Date = (Link[StartDate:StartDate+10])
-         params = (Team,Date,Link,Title)
-         cursor.execute('SELECT * FROM News WHERE (Title=? AND Team=?)',(Title,CurrentTeam))
-         entry = cursor.fetchone()
-         if entry is None:
-             cursor.execute("INSERT INTO News VALUES (NULL, ?, ?, ?, ?)", params)
-             print("    New entry added")
+    try:
+        headers = results[0].findAll('header',attrs={"class":"mod-header"})
+        for i in headers:    
+            hreflink = i.findAll('a')
+            Title = hreflink[0]['title']
+            Link = hreflink[0]['href']    
+            Team = CurrentTeam
+            StartDate = Link.find('2019')
+            Date = (Link[StartDate:StartDate+10])
+            params = (Team,Date,Link,Title)
+            cursor.execute('SELECT * FROM News WHERE (Title=? AND Team=?)',(Title,CurrentTeam))
+            entry = cursor.fetchone()
+            if entry is None:
+                cursor.execute("INSERT INTO News VALUES (NULL, ?, ?, ?, ?)", params)
+                print("    New entry added")
+    except IndexError:
+        print("    Error trying to get news from %s..." %(CurrentTeam))          
 
 conn.commit()
 conn.close()
